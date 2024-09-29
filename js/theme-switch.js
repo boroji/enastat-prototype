12aa6ddad58cc5f7998da794467a2a5d73056fe1
Hossein <contact@borojidesign.ca>
2024-09-29 14:22:28 -0400

    updated buttons

--- js/theme-switch.js
+++ js/theme-switch.js
@@ -1,5 +1,5 @@
 const htmlBody = document.querySelector('body')
-const toggleBtn = document.querySelector('button')
+const toggleBtn = document.querySelector('.button')
 
 toggleBtn.addEventListener('click', () => {
 	htmlBody.classList.toggle('dark-mode')
